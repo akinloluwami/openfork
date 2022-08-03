e6b6cabb3912e175283e557c0565487c265d5cf6
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-08-03 09:37:30 +0100

    removed unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
 import { Box, Text } from "@chakra-ui/react";
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
