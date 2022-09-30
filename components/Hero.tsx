0ec67d61a0602363c050b0defe363d9934da61c7
Timileyin <timileyinoyelekan11@gmail.com>
2022-09-30 20:55:15 +0100

    feat: search bar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,19 +1,28 @@
 import type { NextPage } from "next";
-import { Flex, Box, Heading, Text, Button } from "@chakra-ui/react";
+import {
+  Flex,
+  Box,
+  Heading,
+  Text,
+  Button,
+  Link,
+  Input,
+} from "@chakra-ui/react";
 
 const Hero: NextPage = () => {
   return (
-    <Box p="5%">
+    <Box>
       <Flex
         justify="center"
         textAlign="center"
         align="center"
+        p="5%"
         direction="column"
       >
         <Heading fontSize="50px" maxW="600px" m="auto">
-          Open source has never been easier...
+          Find Open-Source You can contribute to.
         </Heading>
-        <Text maxW="600px" m="10px auto" fontSize="13px">
+        <Text maxW="600px" m="20px auto" fontSize="13px">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas
           dolorem inventore alias assumenda quisquam qui repellat eaque illo
           architecto dolor iur
@@ -23,6 +32,25 @@ const Hero: NextPage = () => {
           {/**LOGO**/} <Text>Sign in with Github</Text>
         </Button>
       </Flex>
+
+      {/* // search */}
+      <Box maxW="500px" m="auto">
+        <Flex align="center" gap="10px" fontWeight="bold" fontSize="13px">
+          <Link>All</Link>
+          <Link>Popular</Link>
+          <Link>Latest</Link>
+        </Flex>
+        <Flex m="10px auto" border="1px solid grey" borderRadius="15px">
+          {/* icon */}
+          <Input
+            outline="none"
+            border="none"
+            p="30px"
+            type="search"
+            placeholder="Type here to search"
+          />
+        </Flex>
+      </Box>
     </Box>
   );
 };
