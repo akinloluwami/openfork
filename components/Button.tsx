a2f4744ddd0e65d2a8b18f4d9e003599a1dd5fa1
Sudo Whoam i <yhoungdev@gmail.com>
2022-09-30 23:08:11 +0100

    Feat: Created Layout for containers

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,8 +1,29 @@
-import type { NextPage } from "next";
-import { Button as Btn } from "@chakra-ui/react";
+// import type { NextPage } from "next";
+// import { Button as Btn } from "@chakra-ui/react";
+import { Button } from '@chakra-ui/react';
 
-const Button: NextPage = () => {
-  return <Btn></Btn>;
-};
+// const Button: NextPage = () => {
+//   return <Btn></Btn>;
+// };
 
-export default Button;
+// export default Button;
+interface Props {
+  children ?: any,
+  bg ?: string,
+  color ?: string
+}
+
+const Buttons =({ children , bg, color }: Props) => {
+  return (
+    <>
+
+      <Button bg={bg} color={color}>
+          {children}
+      </Button>
+    
+    </>
+  )
+}
+
+
+export default Buttons
