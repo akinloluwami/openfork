aa2faca64f467d3f0aac7829fa459052a99f330a
Timileyin <timileyinoyelekan11@gmail.com>
2022-10-01 11:59:22 +0100

    feat: added about and badges section

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Avatar from "../components/Avatar";
 import Button from "../components/Button";
-import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text, Link } from "@chakra-ui/react";
 const Profile: NextPage = () => {
   return (
     <Box p="5%">
@@ -12,7 +12,7 @@ const Profile: NextPage = () => {
             <Text fontSize="12px" fontWeight="thin">
               @username
             </Text>
-            <Heading>Timileyin Oyelekan</Heading>
+            <Heading>Minimal Peace</Heading>
             <Text fontSize="16px">Trying OpenSource</Text>
             <Flex fontSize="13px" gap="10px" m="10px 0">
               <Text>Contributions: 13</Text>
@@ -21,12 +21,12 @@ const Profile: NextPage = () => {
           </Box>
         </Flex>
 
-        <Button color="#000" fontSize="13px">
+        <Button bg="darkorange" fontSize="13px">
           Edit my profile
         </Button>
       </Flex>
 
-      <Box>
+      <Box p="30px 0">
         <Flex gap="15px" m="15px 0">
           <Text
             border="1px solid grey"
@@ -67,11 +67,22 @@ const Profile: NextPage = () => {
         </Flex>
         <Box>
           <Heading fontSize="25px">About</Heading>
-          <Box p="20px 0"></Box>
+          <Box p="20px 0">
+            <Text opacity=".5" fontSize="12px">
+              Bio helps fellow contributors get a better ideal of you.{" "}
+              <Link color="darkorange" opacity=".5" fontSize="12px">
+                Click to Edit
+              </Link>
+            </Text>
+          </Box>
         </Box>
         <Box>
           <Heading fontSize="25px">Badges</Heading>
-          <Box p="20px 0"></Box>
+          <Box p="20px 0">
+            <Text opacity=".5" fontSize="12px">
+              You don't have badges yet
+            </Text>
+          </Box>
         </Box>
       </Box>
     </Box>
