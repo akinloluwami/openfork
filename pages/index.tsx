6767799b0d47104786c3a7a0e210485b08b299b3
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-10-02 18:06:01 +0100

    bleh

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import { Box, ListItem, Text, UnorderedList } from "@chakra-ui/react";
+import { useEffect } from "react";
 import type { NextPage } from "next";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 const Home: NextPage = () => {
+  useEffect(() => {
+    localStorage.setItem("chakra-ui-color-mode", "dark");
+  }, []);
   return (
     <Box>
       <Header />
