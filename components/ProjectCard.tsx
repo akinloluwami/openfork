a06a5ef6e891edca12861cd7db6e5408de0fba88
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-10-02 15:24:46 +0100

    minor fixes

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,16 +1,16 @@
 import type { NextPage } from "next";
 import Tag from "./Tag";
-import { Flex, Box, Heading, Text, Image, Link } from "@chakra-ui/react";
+import { Flex, Box, Heading, Text, Link, Avatar } from "@chakra-ui/react";
 const ProjectCard: NextPage = () => {
   return (
-    <Box p="30px" w="500px" bg="#111" cursor="pointer" borderRadius="30px">
+    <Box p="30px" w="500px" bg="#111" cursor="pointer" borderRadius={"md"}>
       <Flex align="center" gap="10px">
-        <Image src="/avatar.png" width="70px" />
+        <Avatar size={"lg"} />
         <Heading as="h3" fontSize="30px">
-          <Text fontSize="12px" fontWeight="thin" p="0px 2px">
-            <Link> @bossoncode/</Link>
-          </Text>
           <Link textDecoration="underline"> Open-Fork</Link>
+          <Text fontSize="12px" fontWeight="thin" py={2}>
+            @bossoncode
+          </Text>
         </Heading>
       </Flex>
       <Text m="15px 0" fontSize="13px">
