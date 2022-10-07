d94eefa3187f0ff4c317738f09055e78ef21d2cb
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-10-07 11:04:52 +0100

    setup conditional rendering for header right side

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,40 +1,52 @@
 import { Flex, Box, Image, Link, Button, Text, Avatar } from "@chakra-ui/react";
+import { FaGithub } from "react-icons/fa";
 import ContainerLayout from "../Layout/ContainerLayout";
 import { gradient } from "../styles/gradient";
+import { signInWithGithub } from "../utils/supabase/auth";
 import Notifications from "./Notifications";
 import ProfileOptions from "./ProfileOptions";
 const Header = () => {
   return (
     <ContainerLayout>
-      <Flex
-        align="center"
-        py={5}
-        justify="space-between"
-        px="20px"
-      >
+      <Flex align="center" py={5} justify="space-between" px="20px">
         <Image src="/openfork.svg" w={["120px", "150px"]} alt="" />
 
         {/* when logged in */}
-        <Flex align="center" gap="10px">
+        {/* <Flex align="center" gap="10px">
           <Box>
             <Flex gap="20px" justify="space-around" align="center">
               <Notifications />
               <ProfileOptions />
             </Flex>
           </Box>
-        </Flex>
-        {/* when loged out */}
-        {/* <Flex align="center" gap="10px">
-          <Link fontSize="14px">Contribute</Link>
-          <Button
-            leftIcon={<FaGithub />}
-            bgGradient={gradient}
-            variant="solid"
-            size="sm"
-          >
-            Sign In
-          </Button>
         </Flex> */}
+        {/* when loged out */}
+
+        {localStorage.getItem("sb-tebioleiibrvzamyqsia-auth-token") ? (
+          <Flex align="center" gap="10px">
+            <Box>
+              <Flex gap="20px" justify="space-around" align="center">
+                <Notifications />
+                <ProfileOptions />
+              </Flex>
+            </Box>
+          </Flex>
+        ) : (
+          <Flex align="center" gap="10px">
+            <Link fontSize="14px">Contribute</Link>
+            <Button
+              leftIcon={<FaGithub />}
+              bgGradient={gradient}
+              variant="solid"
+              size="sm"
+              onClick={() => {
+                signInWithGithub();
+              }}
+            >
+              Sign In
+            </Button>
+          </Flex>
+        )}
       </Flex>
     </ContainerLayout>
   );
