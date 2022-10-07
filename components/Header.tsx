7ff9aaa7e95824464474a6034a10b30e335b4cf9
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-10-07 12:34:56 +0100

    omoo bugs choke!!!

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,6 @@
 import { Flex, Box, Image, Link, Button, Text, Avatar } from "@chakra-ui/react";
+import userInfo from "../utils/userInfo";
+import { useEffect, useState } from "react";
 import { FaGithub } from "react-icons/fa";
 import ContainerLayout from "../Layout/ContainerLayout";
 import { gradient } from "../styles/gradient";
@@ -6,23 +8,16 @@ import { signInWithGithub } from "../utils/supabase/auth";
 import Notifications from "./Notifications";
 import ProfileOptions from "./ProfileOptions";
 const Header = () => {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    setUser(userInfo());
+  }, []);
   return (
     <ContainerLayout>
       <Flex align="center" py={5} justify="space-between" px="20px">
         <Image src="/openfork.svg" w={["120px", "150px"]} alt="" />
-
-        {/* when logged in */}
-        {/* <Flex align="center" gap="10px">
-          <Box>
-            <Flex gap="20px" justify="space-around" align="center">
-              <Notifications />
-              <ProfileOptions />
-            </Flex>
-          </Box>
-        </Flex> */}
-        {/* when loged out */}
-
-        {localStorage.getItem("sb-tebioleiibrvzamyqsia-auth-token") ? (
+        {user ? (
           <Flex align="center" gap="10px">
             <Box>
               <Flex gap="20px" justify="space-around" align="center">
