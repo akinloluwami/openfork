51c54c5811e67d22dcd20f1e883a45bf452270e5
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-10-07 12:36:23 +0100

    user info

---
+++ utils/userInfo.js
@@ -0,0 +1,11 @@
+const userInfo = () => {
+  if (typeof window !== "undefined") {
+    return (
+      localStorage.getItem("sb-tebioleiibrvzamyqsia-auth-token") &&
+      JSON.parse(localStorage.getItem("sb-tebioleiibrvzamyqsia-auth-token"))
+        .user.user_metadata
+    );
+  }
+};
+
+export default userInfo;
