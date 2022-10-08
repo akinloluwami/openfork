02b315d13fadf4f03d36a0d4ae1ed905b72ba430
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-10-08 18:21:51 +0100

    added new page

---
+++ pages/projects/new.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import Header from "../../components/Header";
+
+const AddNewProject = () => {
+  return (
+    <Box>
+      <Header />
+    </Box>
+  );
+};
+
+export default AddNewProject;
