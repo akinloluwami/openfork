28095adaf4d575049dcdd92f0776e69f5cee6f0a
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-10-08 02:55:27 +0100

    displayed user's name in the title

--- pages/[username].js
+++ pages/[username].js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ContainerLayout from "../Layout/ContainerLayout";
 import Header from "../components/Header";
+import Head from "next/head";
+import userInfo from "../utils/userInfo";
 const Username = () => {
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    setUser(userInfo());
+  }, []);
   return (
     <ContainerLayout>
+      <Head>
+        <title>{user?.name}'s profile on OpenFork | OpenFork</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Header />
     </ContainerLayout>
   );
