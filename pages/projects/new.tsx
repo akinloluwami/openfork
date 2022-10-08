3394f35287f932868c4b04b3d3bb1006b2ad0454
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-10-08 22:14:19 +0100

    all I have to say it, I did a shit ton of things

--- pages/projects/new.tsx
+++ pages/projects/new.tsx
@@ -1,7 +1,10 @@
 import {
   Box,
   Button,
+  Center,
+  CloseButton,
   Flex,
+  Icon,
   Input,
   ListItem,
   Tab,
@@ -12,10 +15,37 @@ import {
   Text,
   UnorderedList,
 } from "@chakra-ui/react";
-import React from "react";
+import { useEffect, useState } from "react";
+import { SiGithub } from "react-icons/si";
 import Header from "../../components/Header";
+import userInfo from "../../utils/userInfo";
+
+interface RepoData {
+  name: string;
+  svn_url: string;
+}
 
 const AddNewProject = () => {
+  const [tabIndex, setTabIndex] = useState(0);
+  const [repos, setRepos] = useState([]);
+  const [query, setQuery] = useState("");
+  const [projectName, setProjectName] = useState("");
+  const [githubURL, setGithubURL] = useState("");
+  const [websiteURL, setWebsiteURL] = useState("");
+  const [description, setDescription] = useState("");
+  const [tags, setTags] = useState(["react", "typescript"]);
+
+  const handleTabsChange = (index: number) => {
+    setTabIndex(index);
+  };
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${userInfo().user_name}/repos`)
+      .then((res) => res.json())
+      .then((data) => {
+        setRepos(data);
+      });
+  }, []);
   return (
     <Box w={"80%"} m={"auto"}>
       <Header />
@@ -40,19 +70,109 @@ const AddNewProject = () => {
           </Box>
         </Flex>
         <Flex w={"50%"} justify={"center"}>
-          <Tabs>
+          <Tabs index={tabIndex} onChange={handleTabsChange}>
             <TabList>
               <Tab>Repository</Tab>
-              <Tab>Details</Tab>
+              <Tab isDisabled={!githubURL}>Details</Tab>
               <Tab>Tech stack</Tab>
             </TabList>
             <TabPanels>
               <TabPanel>
-                <Input placeholder={"Search repository"} my={2} />
-                <Button>Continue</Button>
+                {githubURL === "" && (
+                  <Input
+                    placeholder={"Search repository"}
+                    my={2}
+                    onChange={(e) => {
+                      setQuery(e.target.value);
+                    }}
+                  />
+                )}
+                {githubURL !== "" && (
+                  <Button
+                    bg="linear-gradient(to left, #805ad5 0%, #d53f8c 100%)"
+                    fontSize="13px"
+                    p={0.5}
+                    my={3}
+                    w={350}
+                  >
+                    <Flex
+                      align={"center"}
+                      bg={"#000"}
+                      my={3}
+                      w={"100%"}
+                      h={"100%"}
+                      p={0.5}
+                      cursor={"pointer"}
+                      borderRadius={"5px"}
+                    >
+                      <Icon as={SiGithub} h={5} w={5} mr={2} />
+                      <Text color={"grey.100"}>bossoncode/</Text>
+                      <Text>{projectName}</Text>
+                    </Flex>
+                    <CloseButton
+                      onClick={() => {
+                        setGithubURL("");
+                        setProjectName("");
+                      }}
+                    />
+                  </Button>
+                )}
+                {!query ? (
+                  <></>
+                ) : githubURL !== "" ? (
+                  <></>
+                ) : (
+                  repos
+                    .filter((r: RepoData) =>
+                      r.name.toLowerCase().includes(query.toLowerCase())
+                    )
+                    .map((repo: RepoData) => (
+                      <Button
+                        bg="linear-gradient(to left, #805ad5 0%, #d53f8c 100%)"
+                        fontSize="13px"
+                        p={0.5}
+                        m={3}
+                        w={350}
+                      >
+                        <Flex
+                          align={"center"}
+                          bg={"#000"}
+                          my={3}
+                          w={"100%"}
+                          h={"100%"}
+                          p={0.5}
+                          cursor={"pointer"}
+                          borderRadius={"5px"}
+                          onClick={() => {
+                            setGithubURL(repo.svn_url);
+                            setProjectName(repo.name);
+                            setQuery("");
+                          }}
+                        >
+                          <Icon as={SiGithub} h={5} w={5} mr={2} />
+                          <Text color={"grey.100"}>bossoncode/</Text>
+                          <Text>{repo.name}</Text>
+                        </Flex>
+                      </Button>
+                    ))
+                )}
+                <Box>
+                  <Button
+                    disabled={!githubURL}
+                    onClick={() => {
+                      handleTabsChange(1);
+                    }}
+                  >
+                    Continue
+                  </Button>
+                </Box>
               </TabPanel>
               <TabPanel>
-                <Input placeholder={"Project name"} my={2} />
+                <Input
+                  placeholder={"Project name"}
+                  my={2}
+                  value={projectName}
+                />
                 <Input placeholder={"Project description"} my={2} />
                 <Input placeholder={"Website"} my={2} />
                 <Button>Continue</Button>
