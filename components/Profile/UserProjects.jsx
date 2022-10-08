26f447d8f3c3eb4b5d84cefcf37ceb96df20ffff
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-10-08 17:33:46 +0100

    fetch user projects

--- components/Profile/UserProjects.jsx
+++ components/Profile/UserProjects.jsx
@@ -1,15 +1,42 @@
 import { Box, Button, Center, Flex, Text } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { supabase } from "../../utils/supabaseClient";
 import AddNewProject from "./AddNewProject";
 
 const UserProjects = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      let { data: Projects, error } = await supabase
+        .from("Projects")
+        .select("*")
+        .eq(
+          "user",
+          JSON.parse(localStorage.getItem("sb-tebioleiibrvzamyqsia-auth-token"))
+            .user.id
+        );
+      setProjects(Projects);
+      console.log(Projects);
+    }
+    fetchData();
+  }, []);
+
   return (
     <Flex justify={"center"} align={"center"}>
       <Flex justify={"center"} align={"center"} direction={"column"}>
-        <Text textAlign={"center"}>You don't have any project.</Text>
         <Center>
           <AddNewProject />
         </Center>
+        {projects.length < 1 ? (
+          <Text textAlign={"center"}>You don't have any project.</Text>
+        ) : (
+          projects.map((project) => (
+            <Box key={project.id}>
+              <Text>{project.name}</Text>
+            </Box>
+          ))
+        )}
       </Flex>
     </Flex>
   );
