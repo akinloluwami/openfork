bf2a77c2234aa135a3f5aedbf703b196ab279da7
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-10-08 04:05:26 +0100

    changed styling...

--- pages/[profile].tsx
+++ pages/[profile].tsx
@@ -3,7 +3,17 @@ import Avatar from "../components/Avatar";
 import Header from "../components/Header";
 import Button from "../components/Button";
 import Tag from "../components/Tag";
-import { Box, Flex, Heading, Text, Link } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Heading,
+  Text,
+  Link,
+  Tabs,
+  Tab,
+  TabList,
+  Center,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import userInfo from "../utils/userInfo";
 import Head from "next/head";
@@ -29,56 +39,57 @@ const Profile: NextPage = () => {
       </Head>
       <Header />
       <Box p="25px 10%">
-        <Flex justify="space-between" gap="30px" align="center" wrap="wrap">
-          <Flex align="center" gap="10px">
-            <Avatar src={user?.avatar_url} />
-            <Box>
-              <Text fontSize="12px" fontWeight="thin">
-                @{user?.user_name}
-              </Text>
-              <Heading>{user?.name}</Heading>
-              <Text fontSize="16px">Trying OpenSource</Text>
-              <Flex fontSize="13px" gap="10px" m="10px 0">
-                <Text>Contributions: 13</Text>
-                <Text>upvotes: 230</Text>
-              </Flex>
-            </Box>
+        <Flex justify="center" gap="30px" align="center" wrap="wrap">
+          <Flex align="center">
+            <Flex>
+              <Box mr={10}>
+                <Avatar src={user?.avatar_url} />
+              </Box>
+              <Box>
+                <Heading>{user?.name}</Heading>
+                <Text fontSize="12px" fontWeight="thin">
+                  @{user?.user_name}
+                </Text>
+                <Text fontSize="16px">Trying OpenSource</Text>
+                <Flex fontSize="13px" gap="10px" m="10px 0">
+                  <Text>Contributions: 13</Text>
+                  <Text>upvotes: 230</Text>
+                </Flex>
+              </Box>
+            </Flex>
           </Flex>
-
           <Button
+            ml={20}
             bg="linear-gradient(to left, #805ad5 0%, #d53f8c 100%)"
             fontSize="13px"
+            p={0.5}
+            w={150}
           >
-            Edit my profile
+            <Flex
+              align={"center"}
+              justify={"center"}
+              bg={"#000"}
+              w={"100%"}
+              h={"100%"}
+              p={0.5}
+              borderRadius={"5px"}
+            >
+              <Text>Edit my profile</Text>
+            </Flex>
           </Button>
         </Flex>
 
         <Box p="30px 0">
-          <Flex gap="15px" m="15px 0">
-            <Tag stackName="About" />
-            <Tag stackName="Projects" />
-            <Tag stackName="Upvotes" />
-            <Tag stackName="Bookmarks" />
-          </Flex>
-          <Box>
-            <Heading fontSize="25px">About</Heading>
-            <Box p="20px 0">
-              <Text opacity=".5" fontSize="12px">
-                Bio helps fellow contributors get a better ideal of you.{" "}
-                <Link color="#d53f8c" opacity=".5" fontSize="12px">
-                  Click to Edit
-                </Link>
-              </Text>
-            </Box>
-          </Box>
-          <Box>
-            <Heading fontSize="25px">Badges</Heading>
-            <Box p="20px 0">
-              <Text opacity=".5" fontSize="12px">
-                You do not have badges yet
-              </Text>
-            </Box>
-          </Box>
+          <Center>
+            <Tabs variant={"soft-rounded"} colorScheme={"purple"} size={"sm"}>
+              <TabList>
+                <Tab>About</Tab>
+                <Tab>Projects</Tab>
+                <Tab>Badges</Tab>
+                <Tab>Upvotes</Tab>
+              </TabList>
+            </Tabs>
+          </Center>
         </Box>
       </Box>
     </>
