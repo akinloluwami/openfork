f23872fe22039ffb5139ebe442b7055902a36b56
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-10-08 02:51:05 +0100

    setup dynamic userpage

---
+++ pages/[username].js
@@ -0,0 +1,12 @@
+import React from "react";
+import ContainerLayout from "../Layout/ContainerLayout";
+import Header from "../components/Header";
+const Username = () => {
+  return (
+    <ContainerLayout>
+      <Header />
+    </ContainerLayout>
+  );
+};
+
+export default Username;
