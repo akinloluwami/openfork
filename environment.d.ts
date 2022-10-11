09ac82d612a59da0d7c107a3da342275eb3e5ca5
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-10-11 09:49:31 +0100

    added new variables

--- environment.d.ts
+++ environment.d.ts
@@ -2,5 +2,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_SUPABASE_URL: string;
     NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
+    NEXT_PUBLIC_GITHUB_CLIENT_SECRET: string;
+    NEXT_PUBLIC_GITHUB_TOKEN: string;
   }
 }
