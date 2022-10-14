93a3a7d56fab2ebe53fcdb51f731429c87a1f7a0
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-10-14 09:48:52 +0100

    ....

---
+++ pages/[profile]/projects.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ProfileLayout from "../../Layout/ProfileLayout";
+
+const projects = () => {
+  return <ProfileLayout></ProfileLayout>;
+};
+
+export default projects;
