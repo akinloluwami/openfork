9730cb86915afd887ecc247a0751b61de9ade6db
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-11-08 00:11:32 +0100

    ...

---
+++ pages/my/projects.tsx
@@ -0,0 +1,14 @@
+import { Box, Text } from "@chakra-ui/react";
+import React from "react";
+import Header from "../../components/Header";
+
+const Projects = () => {
+  return (
+    <Box>
+      <Header />
+      <Text>My projects</Text>
+    </Box>
+  );
+};
+
+export default Projects;
