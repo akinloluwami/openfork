052b54cca0b3e3ca6ca69944a4c47f022c462a52
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-11-12 04:27:57 +0100

    added google tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,24 @@ import type { NextPage } from "next";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Projects from "../components/Projects";
+import Head from "next/head"
 const Home: NextPage = () => {
   useEffect(() => {
     localStorage.setItem("chakra-ui-color-mode", "dark");
   }, []);
   return (
     <Box>
+      <Head>
+      <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-GVTLXMTJ30"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-GVTLXMTJ30');
+</script>
+      </Head>
       <Header />
       <Hero />
       <Projects />
