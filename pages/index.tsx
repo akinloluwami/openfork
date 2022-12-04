05e644b5efcd5fa026b160e3a7af42409ea365cc
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-12-04 09:15:59 +0100

    ...

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,11 +10,45 @@ const Home: NextPage = () => {
     localStorage.setItem("chakra-ui-color-mode", "dark");
   }, []);
   return (
-    <Box>
-      <Header />
-      <Hero />
-      <Projects />
-    </Box>
+    <>
+      <Head>
+        <title>
+          Openfork - Opensource projects you can actually contribute to.
+        </title>
+        <meta
+          property="og:image"
+          content="https://tebioleiibrvzamyqsia.supabase.co/storage/v1/object/sign/avatars/openfork_OG.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJhdmF0YXJzL29wZW5mb3JrX09HLnBuZyIsImlhdCI6MTY2ODIzNzQzMCwiZXhwIjoxOTgzNTk3NDMwfQ.cLDjR7WlXJnv2nrL-luqjOgrIGggLuH_XM99-jBVfHA"
+        />
+        <meta
+          property="og:image:secure_url"
+          content="https://tebioleiibrvzamyqsia.supabase.co/storage/v1/object/sign/avatars/openfork_OG.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJhdmF0YXJzL29wZW5mb3JrX09HLnBuZyIsImlhdCI6MTY2ODIzNzQzMCwiZXhwIjoxOTgzNTk3NDMwfQ.cLDjR7WlXJnv2nrL-luqjOgrIGggLuH_XM99-jBVfHA"
+        />
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="627" />
+        <meta
+          property="og:image:alt"
+          content="Openfork - Opensource projects you can actually contribute to."
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@openfork" />
+        <meta name="twitter:title" content="Openfork" />
+        <meta
+          name="twitter:description"
+          content="Opensource project you can actually contribute to."
+        />
+        <meta
+          name="twitter:image"
+          content="https://tebioleiibrvzamyqsia.supabase.co/storage/v1/object/sign/avatars/openfork_OG.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJhdmF0YXJzL29wZW5mb3JrX09HLnBuZyIsImlhdCI6MTY2ODIzNzQzMCwiZXhwIjoxOTgzNTk3NDMwfQ.cLDjR7WlXJnv2nrL-luqjOgrIGggLuH_XM99-jBVfHA"
+        />
+      </Head>
+      <Box>
+        <Header />
+        <Hero />
+        <Projects />
+      </Box>
+    </>
   );
 };
 
