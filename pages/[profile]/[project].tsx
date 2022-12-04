3768c6f2c8a3a92223b4812a738abf222a3170eb
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-12-04 15:26:00 +0100

    added conditional rendering if project doesn't exist

--- pages/[profile]/[project].tsx
+++ pages/[profile]/[project].tsx
@@ -32,15 +32,16 @@ export async function getServerSideProps(context: any) {
     .eq("slug", context.params.project);
   return {
     props: {
-      data: project[0],
+      data: project[0] || null,
     },
   };
 }
 const Project = ({ data }: { data: any }) => {
-  const { id, name, description, github_url, tech_stack } = data;
+  const { id, name, description, github_url, tech_stack } = data || {};
 
   const [upvotes, setUpvotes] = useState<any>([]);
   const [currentUser, setCurrentUser] = useState<any>("");
+
   const getUpvotes = async () => {
     let { data: upvotes }: { data: any } = await supabase
       .from("project_upvotes")
@@ -52,6 +53,7 @@ const Project = ({ data }: { data: any }) => {
   const getCurrentUser = async () => {
     setCurrentUser((await supabase.auth.getUser()).data.user?.id);
   };
+
   useEffect(() => {
     getUpvotes();
     getCurrentUser();
@@ -62,6 +64,7 @@ const Project = ({ data }: { data: any }) => {
       return project.user_id === currentUser;
     });
   }
+
   const upvoteProject = async () => {
     const obj = {
       created_at: new Date(),
@@ -86,61 +89,67 @@ const Project = ({ data }: { data: any }) => {
     }
   };
   return (
-    <div>
-      <Head>
-        <title>
-          {name} - {description}
-        </title>
-      </Head>
-      <Header />
-      <Box w={"60%"} my={2} mx={"auto"}>
-        <Flex justify={"space-between"} align={"center"}>
-          <Box>
-            <Text fontSize={"2rem"} fontWeight={600}>
-              {name}
-            </Text>
-            <Text mb={3} mt={1} fontSize={"1.5rem"} fontWeight={"thin"}>
-              {description}
-            </Text>
+    <>
+      {data === null ? (
+        <>NULL</>
+      ) : (
+        <div>
+          <Head>
+            <title>
+              {name} - {description}
+            </title>
+          </Head>
+          <Header />
+          <Box w={"60%"} my={2} mx={"auto"}>
+            <Flex justify={"space-between"} align={"center"}>
+              <Box>
+                <Text fontSize={"2rem"} fontWeight={600}>
+                  {name}
+                </Text>
+                <Text mb={3} mt={1} fontSize={"1.5rem"} fontWeight={"thin"}>
+                  {description}
+                </Text>
+              </Box>
+              <Flex align={"center"} gap={3}>
+                <Link href={github_url} target="_blank" fontSize={"3xl"}>
+                  <SiGithub />
+                </Link>
+                <Button
+                  size={"lg"}
+                  gap={3}
+                  onClick={() => {
+                    upvoteProject();
+                  }}
+                  bg={
+                    checkUpvoted()
+                      ? ""
+                      : "linear-gradient(to left, #805ad5 0%, #d53f8c 100%)"
+                  }
+                  border={"2px"}
+                  borderColor={checkUpvoted() ? "#d53f8c" : "transparent"}
+                >
+                  <TbArrowBigUpLines />
+                  <Text>
+                    {" "}
+                    {checkUpvoted() ? "Upvoted" : "Upvote"} {upvotes.length}
+                  </Text>
+                </Button>
+              </Flex>
+            </Flex>
+            <Box my={5}>
+              {tech_stack.map((stack: string, i: number) => (
+                <Tag mr={2} size={"lg"} key={i}>
+                  {stack}
+                </Tag>
+              ))}
+            </Box>
+            <Flex align={"center"}>
+              <Flex align={"center"} fontSize={"xl"} gap={1}></Flex>
+            </Flex>
           </Box>
-          <Flex align={"center"} gap={3}>
-            <Link href={github_url} target="_blank" fontSize={"3xl"}>
-              <SiGithub />
-            </Link>
-            <Button
-              size={"lg"}
-              gap={3}
-              onClick={() => {
-                upvoteProject();
-              }}
-              bg={
-                checkUpvoted()
-                  ? ""
-                  : "linear-gradient(to left, #805ad5 0%, #d53f8c 100%)"
-              }
-              border={"2px"}
-              borderColor={checkUpvoted() ? "#d53f8c" : "transparent"}
-            >
-              <TbArrowBigUpLines />
-              <Text>
-                {" "}
-                {checkUpvoted() ? "Upvoted" : "Upvote"} {upvotes.length}
-              </Text>
-            </Button>
-          </Flex>
-        </Flex>
-        <Box my={5}>
-          {tech_stack.map((stack: string, i: number) => (
-            <Tag mr={2} size={"lg"} key={i}>
-              {stack}
-            </Tag>
-          ))}
-        </Box>
-        <Flex align={"center"}>
-          <Flex align={"center"} fontSize={"xl"} gap={1}></Flex>
-        </Flex>
-      </Box>
-    </div>
+        </div>
+      )}
+    </>
   );
 };
 
