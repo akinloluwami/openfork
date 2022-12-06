91ac1e979d0d3cd2737434e94515618ba3ed3f8c
Akinkunmi <akinkunmiisrael41@gmail.com>
2022-12-06 17:42:23 +0100

    fetch project

--- components/ProjectModal.tsx
+++ components/ProjectModal.tsx
@@ -1,7 +1,46 @@
-import { Box, CloseButton, Flex, Heading } from "@chakra-ui/react";
-import React from "react";
+import { Box, CloseButton, Flex, Heading, Text } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import { supabase } from "../utils/supabaseClient";
+
+interface upvoteProps {
+  user_id: string;
+}
+
+interface ProjectProps {
+  id: number;
+  name: string;
+  user?: string;
+  owner?: string;
+  description: string;
+  imgSrc?: any;
+  onOpen?: any;
+  upvotes?: upvoteProps[];
+  upvoteProject?: any;
+  github_url?: string;
+  tech_stack?: any;
+  isUpvoting?: number;
+  slug: string;
+}
 
 const ProjectModal = ({ isOpen, cardClose, projectId }: any) => {
+  const [loading, setLoading] = useState(false);
+  const [project, setProject] = useState<any>({});
+
+  const fetchProject = async () => {
+    setLoading(true);
+    let { data: project }: { data: any } = await supabase
+      .from("projects")
+      .select("*")
+      .eq("id", projectId);
+    console.log(project[0]);
+    setProject(project[0]);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    isOpen && fetchProject();
+  }, [projectId]);
+
   return (
     <>
       <Box
@@ -24,7 +63,11 @@ const ProjectModal = ({ isOpen, cardClose, projectId }: any) => {
         />
 
         <Box w={"90%"} mx={"auto"}>
-          <Heading>{projectId}</Heading>
+          {loading ? (
+            <Text>Loading...</Text>
+          ) : (
+            <Heading>{project.name}</Heading>
+          )}
         </Box>
       </Box>
     </>
