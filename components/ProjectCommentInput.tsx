e4c5b37decaeb994cdac66459fa774d97c6bdb38
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-29 10:48:54 +0100

    fixed indentation

--- components/ProjectCommentInput.tsx
+++ components/ProjectCommentInput.tsx
@@ -1,7 +1,7 @@
-import { Box, Button, Flex, Input,Avatar, Spinner } from "@chakra-ui/react";
+import { Box, Button, Flex, Input, Avatar, Spinner } from "@chakra-ui/react";
 import React, { useState, useRef } from "react";
 import { supabase } from "../utils/supabaseClient";
-import {FaPaperPlane} from "react-icons/fa"
+import { FaPaperPlane } from "react-icons/fa";
 
 const ProjectCommentInput = ({
   userId,
@@ -16,25 +16,46 @@ const ProjectCommentInput = ({
   const [sending, setSending] = useState(false);
 
   const sendComment = async () => {
-    setSending(true)
+    setSending(true);
     const { data, error } = await supabase
       .from("project_comments")
       .insert([
         { project_id: postId, user_id: userId, comment_text: commentText },
       ]);
     setCommentText("");
-    setSending(false)
-
+    setSending(false);
   };
   return (
     <Box>
-    <Flex bg="gray.900" p={2} px={4} gap={2} align="center" my={3} borderRadius="full">
-      <Avatar size="md" />
-    <Input value={commentText} focusBorderColor={"none"} onChange={(e) => setCommentText(e.target.value)} placeholder="What do you think?" bg="none" outline="none" border="none" />
-     <Button size={"lg"} bg={"linear-gradient(to left, #805ad5 0%, #d53f8c 100%)"} onClick={sendComment} my={2} disabled={!commentText || sending}>
-    {sending ? <Spinner />: <FaPaperPlane />}
-      </Button>
-    </Flex>
+      <Flex
+        bg="gray.900"
+        p={2}
+        px={4}
+        gap={2}
+        align="center"
+        my={3}
+        borderRadius="full"
+      >
+        <Avatar size="md" />
+        <Input
+          value={commentText}
+          focusBorderColor={"none"}
+          onChange={(e) => setCommentText(e.target.value)}
+          placeholder="What do you think?"
+          bg="none"
+          outline="none"
+          border="none"
+        />
+        <Button
+          size={"lg"}
+          bg={"linear-gradient(to left, #805ad5 0%, #d53f8c 100%)"}
+          onClick={sendComment}
+          my={2}
+          disabled={!commentText || sending}
+        >
+          {sending ? <Spinner /> : <FaPaperPlane />}
+        </Button>
+      </Flex>
     </Box>
   );
 };
