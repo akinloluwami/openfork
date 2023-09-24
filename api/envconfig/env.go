18fe2891f05423e6e16d7da7af5cc593c510cd16
akinloluwami <akinkunmioye42@gmail.com>
2023-09-24 03:10:29 +0100

    env

---
+++ api/envconfig/env.go
@@ -0,0 +1,35 @@
+package envconfig
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DBHost     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBPort     string
+	DBSSLMode  string
+}
+
+func Load() *Config {
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	config := &Config{
+		DBHost:     os.Getenv("DB_HOST"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBName:     os.Getenv("DB_NAME"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBSSLMode:  os.Getenv("DB_SSLMODE"),
+	}
+
+	return config
+}
