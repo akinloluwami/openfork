88e2e0f7d941e93a9225ca4771583222e24a51d3
akinloluwami <akinkunmioye42@gmail.com>
2023-09-24 09:46:16 +0100

    check if email is take and write data to db

--- api/handlers/auth/signup.go
+++ api/handlers/auth/signup.go
@@ -1,6 +1,8 @@
 package auth
 
 import (
+	"openfork/api/database"
+	"openfork/api/models"
 	"openfork/api/utils"
 
 	"github.com/gofiber/fiber/v2"
@@ -32,6 +34,16 @@ func SignUpHandler(c *fiber.Ctx) error {
 
 	if utils.IsUsernameTaken(form_data.Username) {
 		return c.SendString("Username is already taken")
+	}
+
+	if utils.IsEmailTaken(form_data.Email) {
+		return c.SendString("Email is already taken")
+	}
+
+	result := database.DB.Create(&models.User{Name: form_data.Name, Username: form_data.Username, Email: form_data.Email, Password: utils.HashPassword(form_data.Password)})
+
+	if result.Error != nil {
+		return c.SendString("Error creating user")
 	}
 
 	return c.SendString("Signup endpoint")
