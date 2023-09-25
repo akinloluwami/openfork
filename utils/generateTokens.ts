8cc4a59e0ace0f639cdb6b348c0c0544d779b968
akinloluwami <akinkunmioye42@gmail.com>
2023-09-25 03:53:10 +0100

    !

---
+++ utils/generateTokens.ts
@@ -0,0 +1,59 @@
+import { sign } from "jsonwebtoken";
+import dayjs from "dayjs";
+import prisma from "@/lib/prisma";
+import { JWT_SECRET_KEY } from "@/lib/secrets";
+
+export default async function generateTokens(id: string) {
+  const existingToken = await prisma.refreshToken.findFirst({
+    where: {
+      userId: id,
+    },
+  });
+
+  const hasTokenExpired =
+    existingToken && dayjs().isAfter(dayjs(existingToken?.expiresAt));
+
+  const access_token = sign({ id }, JWT_SECRET_KEY as string, {
+    expiresIn: "15m",
+  });
+
+  const refresh_token =
+    (!hasTokenExpired && existingToken?.token) ||
+    sign({ id }, JWT_SECRET_KEY as string, {
+      expiresIn: "90d",
+    });
+
+  if (!existingToken)
+    await prisma.refreshToken.create({
+      data: {
+        token: refresh_token,
+        expiresAt: dayjs().add(90, "d").toDate(),
+        userId: id,
+      },
+    });
+
+  if (hasTokenExpired) {
+    await prisma.refreshToken.update({
+      where: {
+        userId: id,
+      },
+      data: {
+        token: refresh_token,
+        expiresAt: dayjs().add(90, "d").toDate(),
+      },
+    });
+  }
+
+  console.log("====================================");
+  console.log(
+    `Access Token: ${access_token} | Refresh Token: ${refresh_token}`
+  );
+  console.log("====================================");
+
+  const cookies = {
+    access_token,
+    refresh_token,
+  };
+
+  return cookies;
+}
