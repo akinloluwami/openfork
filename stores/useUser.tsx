d1154b352c6e75c37c78855420666bc529990410
akinloluwami <akinkunmioye42@gmail.com>
2023-09-25 20:03:40 +0100

    okayyyyyy

--- stores/useUser.tsx
+++ stores/useUser.tsx
@@ -6,37 +6,24 @@ interface IStore {
   name: string;
   username: string;
   image: string;
-  setStore: (store: IStore) => void;
 }
 
 export const useUser = create<IStore>((set) => {
-  const storedData = localStorage.getItem("openfork_user");
-  const initialData = storedData ? JSON.parse(storedData) : {};
-
   const initialState: IStore = {
-    id: initialData.id || "",
-    name: initialData.name || "",
-    username: initialData.username || "",
-    image: initialData.image || "",
-    setStore: (store: IStore) => {
-      set(store);
-      localStorage.setItem("openfork_user", JSON.stringify(store));
-    },
+    id: "",
+    name: "",
+    username: "",
+    image: "",
   };
 
-  if (!initialData.name || !initialData.username || !initialData.image) {
-    axios("/auth")
-      .then((response) => {
-        const data = response.data;
-
-        initialState.setStore(data);
-
-        localStorage.setItem("openfork_user", JSON.stringify(data));
-      })
-      .catch((error) => {
-        console.error("Error fetching data from /auth:", error);
-      });
-  }
+  axios("/auth")
+    .then((response) => {
+      const data = response.data;
+      set(data);
+    })
+    .catch((error) => {
+      console.error("Error fetching data from /auth:", error);
+    });
 
   return initialState;
 });
