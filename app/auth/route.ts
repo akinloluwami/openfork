bbabc7b5c0ced2d08d02652b074271f1b0f9af00
akinloluwami <akinkunmioye42@gmail.com>
2023-09-25 18:51:22 +0100

    check if token has expired

--- app/auth/route.ts
+++ app/auth/route.ts
@@ -1,5 +1,5 @@
 import { JWT_SECRET_KEY } from "@/lib/secrets";
-import { verify } from "jsonwebtoken";
+import { verify, TokenExpiredError } from "jsonwebtoken";
 import { cookies } from "next/headers";
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
@@ -10,23 +10,29 @@ export async function GET(request: Request) {
 
   if (!token) return new Response("No token", { status: 401 });
 
-  const jwt = verify(token.value, JWT_SECRET_KEY!) as { id: string };
+  try {
+    const jwt = verify(token.value, JWT_SECRET_KEY!) as { id: string };
 
-  console.log(jwt);
+    const user = await prisma.user.findUnique({
+      where: { id: jwt.id },
+    });
 
-  const user = await prisma.user.findUnique({
-    where: { id: jwt.id },
-  });
+    if (!user) return new Response("Invalid user id", { status: 401 });
 
-  if (!user) return new Response("Invalid user id", { status: 401 });
-
-  return NextResponse.json({
-    id: user.id,
-    name: user.name,
-    email: user.email,
-    username: user.username,
-    isVerified: user.isVerified,
-    image: user.image,
-    github: user.githubUrl,
-  });
+    return NextResponse.json({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      username: user.username,
+      isVerified: user.isVerified,
+      image: user.image,
+      github: user.githubUrl,
+    });
+  } catch (error) {
+    if (error instanceof TokenExpiredError) {
+      return new Response("Token has expired", { status: 401 });
+    } else {
+      return new Response("Invalid token", { status: 401 });
+    }
+  }
 }
