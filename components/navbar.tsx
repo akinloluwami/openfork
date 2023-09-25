1092771a0c34fc4a24693430de02307936e1c9de
akinloluwami <akinkunmioye42@gmail.com>
2023-09-25 19:01:42 +0100

    get user info

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,12 @@
+"use client";
+
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { SiGithub } from "react-icons/si";
+import { useUser } from "@/stores/useUser";
 
 export default function Navbar() {
+  const { id, name, image, username } = useUser();
   return (
     <div className="flex items-center justify-between py-5 px-10">
       <div className="flex items-center gap-20">
@@ -14,12 +18,16 @@ export default function Navbar() {
           <Link href="/people">People</Link>
         </div>
       </div>
-      <Link href={"/auth/github"}>
-        <Button>
-          <SiGithub className="mr-2" />
-          Connect with GitHub
-        </Button>
-      </Link>
+      {id ? (
+        <></>
+      ) : (
+        <Link href={"/auth/github"}>
+          <Button>
+            <SiGithub className="mr-2" />
+            Connect with GitHub
+          </Button>
+        </Link>
+      )}
     </div>
   );
 }
