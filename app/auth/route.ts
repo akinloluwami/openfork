200bc69e8b1c444864c83e095174f5af6a6b5ab3
akinloluwami <akinkunmioye42@gmail.com>
2023-09-25 18:43:36 +0100

    return user data

--- app/auth/route.ts
+++ app/auth/route.ts
@@ -1,6 +1,8 @@
 import { JWT_SECRET_KEY } from "@/lib/secrets";
 import { verify } from "jsonwebtoken";
 import { cookies } from "next/headers";
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const cookieStore = cookies();
@@ -8,11 +10,23 @@ export async function GET(request: Request) {
 
   if (!token) return new Response("No token", { status: 401 });
 
-  const jwt = verify(token.value, JWT_SECRET_KEY!);
+  const jwt = verify(token.value, JWT_SECRET_KEY!) as { id: string };
 
   console.log(jwt);
 
-  return new Response("Auth!", {
-    status: 200,
+  const user = await prisma.user.findUnique({
+    where: { id: jwt.id },
+  });
+
+  if (!user) return new Response("Invalid user id", { status: 401 });
+
+  return NextResponse.json({
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    username: user.username,
+    isVerified: user.isVerified,
+    image: user.image,
+    github: user.githubUrl,
   });
 }
