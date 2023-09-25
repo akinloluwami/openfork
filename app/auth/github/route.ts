a411a6987a114dcf1f351df895e0cd2809ecd74a
akinloluwami <akinkunmioye42@gmail.com>
2023-09-25 14:47:13 +0100

    fixed return

--- app/auth/github/route.ts
+++ app/auth/github/route.ts
@@ -1,8 +1,13 @@
 import { GITHUB_CLIENT_ID } from "@/lib/secrets";
-import { redirect } from "next/navigation";
 
 export async function GET(req: Request) {
-  redirect(
-    `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=user:email`
-  );
+  return new Response("Redirecting...", {
+    status: 302,
+    headers: {
+      Location:
+        "https://github.com/login/oauth/authorize?client_id=" +
+        GITHUB_CLIENT_ID +
+        "&scope=user:email",
+    },
+  });
 }
