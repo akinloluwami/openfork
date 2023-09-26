d3294df200270bcb7f7c02e080881a20ad1963cf
akinloluwami <akinkunmioye42@gmail.com>
2023-09-26 19:12:52 +0100

    cooler way to set cookies

--- utils/generateTokens.ts
+++ utils/generateTokens.ts
@@ -2,52 +2,78 @@ import { sign } from "jsonwebtoken";
 import dayjs from "dayjs";
 import prisma from "@/lib/prisma";
 import { JWT_SECRET_KEY } from "@/lib/secrets";
+import { cookies } from "next/headers";
 
 export default async function generateTokens(id: string) {
-  const existingToken = await prisma.refreshToken.findFirst({
-    where: {
-      userId: id,
-    },
-  });
-
-  const hasTokenExpired =
-    existingToken && dayjs().isAfter(dayjs(existingToken?.expiresAt));
-
-  const access_token = sign({ id }, JWT_SECRET_KEY as string, {
-    expiresIn: "15m",
-  });
-
-  const refresh_token =
-    (!hasTokenExpired && existingToken?.token) ||
-    sign({ id }, JWT_SECRET_KEY as string, {
-      expiresIn: "90d",
-    });
-
-  if (!existingToken)
-    await prisma.refreshToken.create({
-      data: {
-        token: refresh_token,
-        expiresAt: dayjs().add(90, "d").toDate(),
-        userId: id,
-      },
-    });
-
-  if (hasTokenExpired) {
-    await prisma.refreshToken.update({
+  try {
+    const existingToken = await prisma.refreshToken.findFirst({
       where: {
         userId: id,
       },
-      data: {
-        token: refresh_token,
-        expiresAt: dayjs().add(90, "d").toDate(),
+    });
+
+    const hasTokenExpired =
+      existingToken && dayjs().isAfter(dayjs(existingToken?.expiresAt));
+
+    const access_token = sign({ id }, JWT_SECRET_KEY as string, {
+      expiresIn: "15m",
+    });
+
+    const refresh_token =
+      (!hasTokenExpired && existingToken?.token) ||
+      sign({ id }, JWT_SECRET_KEY as string, {
+        expiresIn: "90d",
+      });
+
+    if (!existingToken)
+      await prisma.refreshToken.create({
+        data: {
+          token: refresh_token,
+          expiresAt: dayjs().add(90, "d").toDate(),
+          userId: id,
+        },
+      });
+
+    if (hasTokenExpired) {
+      await prisma.refreshToken.update({
+        where: {
+          userId: id,
+        },
+        data: {
+          token: refresh_token,
+          expiresAt: dayjs().add(90, "d").toDate(),
+        },
+      });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
       },
     });
-  }
 
-  const cookies = {
-    access_token,
-    refresh_token,
-  };
+    const openforkUserJSON = {
+      id: user?.id,
+      name: user?.name,
+      email: user?.email,
+      isVerified: user?.isVerified,
+      github: user?.githubUrl,
+      image: user?.image,
+    };
 
-  return cookies;
+    const cook = cookies();
+
+    cook.set("access_token", access_token, {
+      httpOnly: true,
+      expires: dayjs().add(15, "m").toDate(),
+    });
+    cook.set("refresh_token", refresh_token, {
+      httpOnly: true,
+      expires: dayjs().add(90, "d").toDate(),
+    });
+
+    cook.set("openfork_user", JSON.stringify(openforkUserJSON), {
+      expires: dayjs().add(90, "d").toDate(),
+    });
+  } catch (error) {}
 }
