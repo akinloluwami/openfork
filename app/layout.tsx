2c5894fa6dd1d3e20cb4c9713cc5533042e733db
akinloluwami <akinkunmioye42@gmail.com>
2023-09-27 20:36:38 +0100

    render children

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
+        <div className="">{children}</div>
       </body>
     </html>
   );
