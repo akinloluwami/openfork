17c01ee290c600fcff7a42ef4000e6c0a203783c
akinloluwami <akinkunmioye42@gmail.com>
2023-09-28 13:54:58 +0100

    read:org

--- app/auth/github/route.ts
+++ app/auth/github/route.ts
@@ -1,13 +1,14 @@
 import { GITHUB_CLIENT_ID } from "@/lib/secrets";
 
 export async function GET(req: Request) {
+  const scope = "user:email read:org";
+
+  const authorizationUrl = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=${scope}`;
+
   return new Response("Redirecting...", {
     status: 302,
     headers: {
-      Location:
-        "https://github.com/login/oauth/authorize?client_id=" +
-        GITHUB_CLIENT_ID +
-        "&scope=user:email",
+      Location: authorizationUrl,
     },
   });
 }
