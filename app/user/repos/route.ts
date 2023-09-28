3c697dd455a086d14d93564eb6172e86d6f99f5a
akinloluwami <akinkunmioye42@gmail.com>
2023-09-28 12:45:33 +0100

    get user github access token

--- app/user/repos/route.ts
+++ app/user/repos/route.ts
@@ -1,9 +1,18 @@
 import { getUserId } from "@/utils/getUserIdFromToken";
+import prisma from "@/lib/prisma";
 
 export async function GET(request: Request) {
   const userId = await getUserId();
 
   if (!userId) return new Response("No user id", { status: 401 });
 
-  return new Response(userId, { status: 200 });
+  const github = await prisma.github.findUnique({
+    where: {
+      userId,
+    },
+  });
+
+  const accessToken = github?.accessToken;
+
+  return new Response(accessToken, { status: 200 });
 }
