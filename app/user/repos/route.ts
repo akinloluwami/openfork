7f9abeb0ed96c782e3b1a87d6e91056e989a6653
akinloluwami <akinkunmioye42@gmail.com>
2023-09-28 13:54:31 +0100

    fetch all user repos and organization repos

--- app/user/repos/route.ts
+++ app/user/repos/route.ts
@@ -1,5 +1,7 @@
 import { getUserId } from "@/utils/getUserIdFromToken";
 import prisma from "@/lib/prisma";
+import { Octokit } from "@octokit/rest";
+import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const userId = await getUserId();
@@ -14,5 +16,52 @@ export async function GET(request: Request) {
 
   const accessToken = github?.accessToken;
 
-  return new Response(accessToken, { status: 200 });
+  const octokit = new Octokit({
+    auth: accessToken,
+  });
+
+  const { data } = await octokit.request("GET /user");
+  const username = data.login;
+
+  const userRepos = await octokit.repos.listForUser({
+    username,
+    per_page: 100,
+  });
+
+  const orgs = await octokit.request("GET /user/orgs");
+
+  let userOrgsRepos: any = [
+    {
+      org: username,
+      repos: userRepos.data.map((repo: any) => ({
+        name: repo.name,
+        html_url: repo.html_url,
+        full_name: repo.full_name,
+        description: repo.description,
+      })),
+    },
+  ];
+
+  async function fetchOrgRepos() {
+    for (const org of orgs.data) {
+      const reposResponse = await octokit.request(
+        `GET /orgs/${org.login}/repos`
+      );
+      const repos = reposResponse.data.map((repo: any) => ({
+        name: repo.name,
+        html_url: repo.html_url,
+        full_name: repo.full_name,
+        description: repo.description,
+      }));
+
+      userOrgsRepos.push({
+        org: org.login,
+        repos: repos,
+      });
+    }
+  }
+
+  await fetchOrgRepos();
+
+  return NextResponse.json(userOrgsRepos, { status: 200 });
 }
