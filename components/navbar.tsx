b659587bdc07f4a63d0806d44e64b8d0379af522
akinloluwami <akinkunmioye42@gmail.com>
2023-09-28 22:53:50 +0100

    added background color and sticky positioning

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -17,7 +17,7 @@ import Logout from "./logout";
 export default function Navbar() {
   const { id, name, image, username } = useUser();
   return (
-    <div className="flex items-center justify-between h-20 px-10 border-b">
+    <div className="flex items-center justify-between h-20 px-10 border-b sticky top-0 z-10 bg-white">
       <div className="flex items-center gap-20">
         <div className="">
           <h1 className="font-semibold text-2xl">Openfork</h1>
