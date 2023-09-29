104ff79285d0e3c4216c480e9499bce7294e8843
akinloluwami <akinkunmioye42@gmail.com>
2023-09-29 21:55:11 +0100

    bruh! 💀

---
+++ lib/tech-stack.ts
@@ -0,0 +1,451 @@
+import {
+  SiAdonisjs,
+  SiAiohttp,
+  SiAlpinedotjs,
+  SiAmazonaws,
+  SiAmp,
+  SiAntdesign,
+  SiApachecordova,
+  SiApostrophe,
+  SiAppveyor,
+  SiArangodb,
+  SiAtom,
+  SiAuth0,
+  SiAuthy,
+  SiBabel,
+  SiBlazor,
+  SiBootstrap,
+  SiBulma,
+  SiC,
+  SiCakephp,
+  SiChakraui,
+  SiChartdotjs,
+  SiCodeceptjs,
+  SiCodeigniter,
+  SiCoffeescript,
+  SiCsharp,
+  SiCss3,
+  SiD3Dotjs,
+  SiDart,
+  SiDjango,
+  SiDotnet,
+  SiElectron,
+  SiEmberdotjs,
+  SiErlang,
+  SiExpo,
+  SiFastapi,
+  SiFirebase,
+  SiFlask,
+  SiFlutter,
+  SiGatsby,
+  SiGo,
+  SiGraphql,
+  SiGreensock,
+  SiGulp,
+  SiHtml5,
+  SiHugo,
+  SiIonic,
+  SiJavascript,
+  SiJest,
+  SiJson,
+  SiJsonwebtokens,
+  SiKotlin,
+  SiLaravel,
+  SiMeteor,
+  SiMocha,
+  SiMongodb,
+  SiMui,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiNpm,
+  SiOpenai,
+  SiP5Dotjs,
+  SiPhp,
+  SiPlanetscale,
+  SiPostgresql,
+  SiPrisma,
+  SiPython,
+  SiReact,
+  SiRust,
+  SiSass,
+  SiSocketdotio,
+  SiStrapi,
+  SiSupabase,
+  SiSvelte,
+  SiSymfony,
+  SiTailwindcss,
+  SiTensorflow,
+  SiThreedotjs,
+  SiTypescript,
+  SiVercel,
+  SiVuedotjs,
+  SiYarn,
+} from "react-icons/si";
+import { IconType } from "react-icons";
+import { FaJava } from "react-icons/fa";
+
+interface techStack {
+  name: string;
+  logo: IconType;
+}
+
+const ts: techStack[] = [
+  {
+    name: "JavaScript",
+    logo: SiJavascript,
+  },
+  {
+    name: "PHP",
+    logo: SiPhp,
+  },
+  {
+    name: "React",
+    logo: SiReact,
+  },
+  {
+    name: "Vue",
+    logo: SiVuedotjs,
+  },
+  {
+    name: "Svelte",
+    logo: SiSvelte,
+  },
+  {
+    name: "Alpine.js",
+    logo: SiAlpinedotjs,
+  },
+  {
+    name: "Go",
+    logo: SiGo,
+  },
+  {
+    name: "Typescript",
+    logo: SiTypescript,
+  },
+  {
+    name: "Rust",
+    logo: SiRust,
+  },
+  {
+    name: "Next.js",
+    logo: SiNextdotjs,
+  },
+  {
+    name: "HTML",
+    logo: SiHtml5,
+  },
+  {
+    name: "CSS",
+    logo: SiCss3,
+  },
+  {
+    name: "Supabase",
+    logo: SiSupabase,
+  },
+  {
+    name: "Firebase",
+    logo: SiFirebase,
+  },
+  {
+    name: "Tailwind",
+    logo: SiTailwindcss,
+  },
+  {
+    name: "Material UI",
+    logo: SiMui,
+  },
+  {
+    name: "Chakra UI",
+    logo: SiChakraui,
+  },
+  {
+    name: "Chart.js",
+    logo: SiChartdotjs,
+  },
+  {
+    name: "Python",
+    logo: SiPython,
+  },
+  {
+    name: "Laravel",
+    logo: SiLaravel,
+  },
+  {
+    name: "Flutter",
+    logo: SiFlutter,
+  },
+  {
+    name: "React Native",
+    logo: SiReact,
+  },
+  {
+    name: "Expo",
+    logo: SiExpo,
+  },
+  {
+    name: "JSON",
+    logo: SiJson,
+  },
+  {
+    name: "JSON Web tokens",
+    logo: SiJsonwebtokens,
+  },
+  {
+    name: "Three.js",
+    logo: SiThreedotjs,
+  },
+  {
+    name: "Ionic",
+    logo: SiIonic,
+  },
+  {
+    name: "Bootstrap",
+    logo: SiBootstrap,
+  },
+  {
+    name: "Babel",
+    logo: SiBabel,
+  },
+  {
+    name: "NPM",
+    logo: SiNpm,
+  },
+  {
+    name: "Yarn",
+    logo: SiYarn,
+  },
+  {
+    name: "Strapi",
+    logo: SiStrapi,
+  },
+  {
+    name: "Chart.js",
+    logo: SiChartdotjs,
+  },
+  {
+    name: "Flask",
+    logo: SiFlask,
+  },
+  {
+    name: "Django",
+    logo: SiDjango,
+  },
+  {
+    name: "Dart",
+    logo: SiDart,
+  },
+  {
+    name: "SCSS",
+    logo: SiSass,
+  },
+  {
+    name: "SASS",
+    logo: SiSass,
+  },
+  {
+    name: "Bulma",
+    logo: SiBulma,
+  },
+  {
+    name: "Socket.IO",
+    logo: SiSocketdotio,
+  },
+  {
+    name: "Jest",
+    logo: SiJest,
+  },
+  {
+    name: "Mocha",
+    logo: SiMocha,
+  },
+  {
+    name: "Ember.js",
+    logo: SiEmberdotjs,
+  },
+  {
+    name: "Gulp",
+    logo: SiGulp,
+  },
+  {
+    name: "Node.js",
+    logo: SiNodedotjs,
+  },
+  {
+    name: "Express.js",
+    logo: SiGatsby,
+  },
+  {
+    name: "TensorFlow",
+    logo: SiTensorflow,
+  },
+  {
+    name: "CodeIgniter",
+    logo: SiCodeigniter,
+  },
+  {
+    name: "Symfony",
+    logo: SiSymfony,
+  },
+  {
+    name: "Meteor",
+    logo: SiMeteor,
+  },
+  {
+    name: "D3.js",
+    logo: SiD3Dotjs,
+  },
+  {
+    name: "p5.js",
+    logo: SiP5Dotjs,
+  },
+  {
+    name: "GraphQL",
+    logo: SiGraphql,
+  },
+  {
+    name: "MongoDB",
+    logo: SiMongodb,
+  },
+  {
+    name: "Babel",
+    logo: SiBabel,
+  },
+  {
+    name: "OpenAI",
+    logo: SiOpenai,
+  },
+  {
+    name: "GSAP",
+    logo: SiGreensock,
+  },
+  {
+    name: "Gatsby",
+    logo: SiGatsby,
+  },
+  {
+    name: "Hugo",
+    logo: SiHugo,
+  },
+  {
+    name: ".NET",
+    logo: SiDotnet,
+  },
+  {
+    name: "Dot NET",
+    logo: SiDotnet,
+  },
+  {
+    name: "Adonis.js",
+    logo: SiAdonisjs,
+  },
+  {
+    name: "AIOHTTP",
+    logo: SiAiohttp,
+  },
+  {
+    name: "AMP",
+    logo: SiAmp,
+  },
+  {
+    name: "AntDesign",
+    logo: SiAntdesign,
+  },
+  {
+    name: "Cordova",
+    logo: SiApachecordova,
+  },
+  {
+    name: "Apstrophe",
+    logo: SiApostrophe,
+  },
+  {
+    name: "Appveyor",
+    logo: SiAppveyor,
+  },
+  {
+    name: "ArangoDB",
+    logo: SiArangodb,
+  },
+  {
+    name: "Atom",
+    logo: SiAtom,
+  },
+  {
+    name: "Electron",
+    logo: SiElectron,
+  },
+  {
+    name: "Authy",
+    logo: SiAuthy,
+  },
+  {
+    name: "Auth0",
+    logo: SiAuth0,
+  },
+  {
+    name: "Java",
+    logo: FaJava,
+  },
+  {
+    name: "Kotlin",
+    logo: SiKotlin,
+  },
+  {
+    name: "Blazor",
+    logo: SiBlazor,
+  },
+  {
+    name: "C#",
+    logo: SiCsharp,
+  },
+  {
+    name: "CSharp",
+    logo: SiCsharp,
+  },
+  {
+    name: "C",
+    logo: SiC,
+  },
+  {
+    name: "CakePHP",
+    logo: SiCakephp,
+  },
+  {
+    name: "Codecept.js",
+    logo: SiCodeceptjs,
+  },
+  {
+    name: "Coffeescript",
+    logo: SiCoffeescript,
+  },
+  {
+    name: "Erlang",
+    logo: SiErlang,
+  },
+  {
+    name: "FastAPI",
+    logo: SiFastapi,
+  },
+  {
+    name: "Prisma",
+    logo: SiPrisma,
+  },
+  {
+    name: "Postgres",
+    logo: SiPostgresql,
+  },
+  {
+    name: "Planetscale",
+    logo: SiPlanetscale,
+  },
+  {
+    name: "AWS",
+    logo: SiAmazonaws,
+  },
+  {
+    name: "Vercel",
+    logo: SiVercel,
+  },
+];
+
+export { ts as techStack };
