ed265524a5bf5d33f6088d77c01acfb12fe88071
akinloluwami <akinkunmioye42@gmail.com>
2023-09-30 14:04:39 +0100

    return json response

--- app/project/route.ts
+++ app/project/route.ts
@@ -5,48 +5,68 @@ import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-  const userId = await getUserId();
+  try {
+    const userId = await getUserId();
 
-  if (!userId) return new Response("No user id", { status: 401 });
+    if (!userId)
+      return NextResponse.json({ error: "No user id" }, { status: 401 });
 
-  const { name, description, website, repository, techStack } =
-    (await request.json()) as NewProjectProps;
+    const { name, description, website, repository, techStack } =
+      (await request.json()) as NewProjectProps;
 
-  if (!name) return new Response("Project name is required", { status: 400 });
+    if (!name)
+      return NextResponse.json(
+        { error: "Project name is required" },
+        { status: 400 }
+      );
 
-  if (!description)
-    return new Response("Project description is required", { status: 400 });
+    if (!description)
+      return NextResponse.json(
+        { error: "Project description is required" },
+        { status: 400 }
+      );
 
-  if (website && !validator.isURL(website))
-    return new Response("Invalid website URL", {
-      status: 400,
+    if (website && !validator.isURL(website))
+      return NextResponse.json(
+        { error: "Invalid website URL" },
+        { status: 400 }
+      );
+
+    if (!repository)
+      return NextResponse.json(
+        { error: "Repository URL is required" },
+        { status: 400 }
+      );
+
+    if (!validator.isURL(repository) || !repository.includes("github.com"))
+      return NextResponse.json(
+        { error: "Invalid repository URL" },
+        { status: 400 }
+      );
+
+    if (techStack.length === 0)
+      return NextResponse.json(
+        { error: "Tech stack is required" },
+        { status: 400 }
+      );
+
+    const newProject = await prisma.project.create({
+      data: {
+        name,
+        description,
+        website,
+        repository,
+        techStack,
+        userId,
+      },
     });
 
-  if (!repository)
-    return new Response("Repository URL is required", { status: 400 });
-
-  if (!validator.isURL(repository) || !repository.includes("github.com"))
-    return new Response("Invalid repository URL", {
-      status: 400,
-    });
-
-  if (techStack.length === 0)
-    return new Response("Tech stack is required", { status: 400 });
-
-  const newProject = await prisma.project.create({
-    data: {
-      name,
-      description,
-      website,
-      repository,
-      techStack,
-      userId,
-    },
-  });
-
-  return NextResponse.json({ success: true, newProject });
+    return NextResponse.json({ success: true, newProject });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export async function GET() {
-  return new Response("Hello world", { status: 200 });
+  return NextResponse.json({ message: "Hello world" }, { status: 200 });
 }
