a95ba33a0d0b040bcf9a3d157b9e56275733a8b8
akinloluwami <akinkunmioye42@gmail.com>
2023-09-30 23:21:50 +0100

    generate dynamic page title

--- app/p/[slug]/page.tsx
+++ app/p/[slug]/page.tsx
@@ -1,9 +1,20 @@
-import { Button } from "@/components/ui/button";
 import { getProject } from "@/lib/api/getProject";
 import { ProjectProps } from "@/types";
-import Head from "next/head";
+import { Metadata } from "next";
 import Link from "next/link";
 import { SiGithub } from "react-icons/si";
+
+export async function generateMetadata({
+  params: { slug },
+}: {
+  params: { slug: string };
+}) {
+  const project: ProjectProps = await getProject(slug);
+  return {
+    title: `${project.name} • Openfork`,
+    description: project.description,
+  } as Metadata;
+}
 
 export default async function Project({
   params: { slug },
@@ -14,9 +25,6 @@ export default async function Project({
 
   return (
     <div>
-      <Head>
-        <title>{project.name} • Openfork</title>
-      </Head>
       <div className="max-w-[800px] mx-auto mt-10">
         <div className="flex flex-col gap-2">
           <div className="h-20 w-20 bg-orange-500 rounded-lg"></div>
