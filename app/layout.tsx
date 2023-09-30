c78b8784cedef41efe2f4cd9ff7ac33d3aecbc88
akinloluwami <akinkunmioye42@gmail.com>
2023-09-30 11:11:23 +0100

    ___

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/navbar";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Toaster />
       <body className={inter.className}>
         <Navbar />
         <div className="">{children}</div>
