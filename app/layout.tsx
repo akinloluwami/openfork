203ebdfbf9c7ad27d93f210501c170ca4ab13858
akinloluwami <akinkunmioye42@gmail.com>
2023-09-30 14:04:16 +0100

    replaced with react-host-toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/navbar";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
