48f1adf5927484bdb970242ba3d70c1f84b4388b
akinloluwami <akinkunmioye42@gmail.com>
2023-09-30 21:48:32 +0100

    removed toaster component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/navbar";
-import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
