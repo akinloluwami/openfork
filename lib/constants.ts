cfeddde4c4d9be238f25a8bc008dad6bb52ff60d
akinloluwami <akinkunmioye42@gmail.com>
2023-09-30 21:20:38 +0100

    export DOMAIN

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,5 @@
 const isProd = process.env.NODE_ENV === "production";
 
-export { isProd };
+const DOMAIN = isProd ? "https://v2.openfork.dev" : "http://localhost:1800";
+
+export { isProd, DOMAIN };
