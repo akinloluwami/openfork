75c84229a765cb280712a88387d8cf666a4e7b73
akinloluwami <akinkunmioye42@gmail.com>
2023-10-30 00:29:57 +0100

    feat: added card content

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -1,7 +1,11 @@
+import { Avatar } from "./ui/avatar";
+import Upvotebutton from "./upvote-button";
+
 const ProjectCard = ({
   project,
 }: {
   project: {
+    id: string;
     name: string;
     slug: string;
     description: string;
@@ -12,10 +16,21 @@ const ProjectCard = ({
     };
   };
 }) => {
-  const { name, description, slug, upvotes, user } = project;
+  const { id, name, description, slug, upvotes, user } = project;
   const { username, isVerified } = user;
 
-  return <div>{name}</div>;
+  return (
+    <div className="border-2 border-black rounded-lg w-[300px] p-2">
+      <h2 className="font-semibold text-xl">{name}</h2>
+      <p className="mt-3">{description}</p>
+      <div className="flex mt-3">
+        <p className="font-semibold text-sm">{username}</p>
+      </div>
+      <div className="mt-10">
+        <Upvotebutton projectId={id} />
+      </div>
+    </div>
+  );
 };
 
 export default ProjectCard;
