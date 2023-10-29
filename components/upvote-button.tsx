ba1db77cf6ae1be414b369dbaab2fd40b7b6fec3
akinloluwami <akinkunmioye42@gmail.com>
2023-10-29 11:52:05 +0100

    fetch upvote status

--- components/upvote-button.tsx
+++ components/upvote-button.tsx
@@ -1,8 +1,18 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { Button } from "./ui/button";
+import { axios } from "@/lib/axios";
 
 const Upvotebutton = ({ id }: { id: string }) => {
+  const [hasUpvoted, setHasUpvoted] = useState(false);
+
+  useEffect(() => {
+    axios(`/project/${id}/upvote`).then((data) => {
+      setHasUpvoted(data.data.hasUpvoted);
+    });
+  }, []);
+
   return <Button className="w-[250px] py-6"> Upvote</Button>;
 };
 
