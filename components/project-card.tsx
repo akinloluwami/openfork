77b45ee29d903961d0e2886cec04bca3f87ed482
akinloluwami <akinkunmioye42@gmail.com>
2023-10-29 23:50:40 +0100

    feat: destructure project props

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -1,19 +1,20 @@
 const ProjectCard = ({
-  name,
-  slug,
-  description,
-  upvotes,
-  user,
+  project,
 }: {
-  name: string;
-  slug: string;
-  description: string;
-  upvotes: number;
-  user: {
-    username: string;
-    isVerified: boolean;
+  project: {
+    name: string;
+    slug: string;
+    description: string;
+    upvotes: number;
+    user: {
+      username: string;
+      isVerified: boolean;
+    };
   };
 }) => {
+  const { name, description, slug, upvotes, user } = project;
+  const { username, isVerified } = user;
+
   return <div>ProjectCard</div>;
 };
 
