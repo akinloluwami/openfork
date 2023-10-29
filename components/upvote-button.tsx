0fca5a0e5c83d9a28bed2102d47a2e3057773309
akinloluwami <akinkunmioye42@gmail.com>
2023-10-29 11:45:44 +0100

    ..

---
+++ components/upvote-button.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { Button } from "./ui/button";
+
+const Upvotebutton = ({ id }: { id: string }) => {
+  return <Button className="w-[250px] py-6"> Upvote</Button>;
+};
+
+export default Upvotebutton;
