924089a8a7c3db241266813600694930eb221ad9
akinloluwami <akinkunmioye42@gmail.com>
2023-10-29 11:45:40 +0100

    feat: upvote endpoint

---
+++ app/api/project/[id]/upvote/route.ts
@@ -0,0 +1,109 @@
+import { getUserId } from "@/utils/getUserIdFromToken";
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function POST(
+  request: Request,
+  {
+    params,
+  }: {
+    params: { id: string };
+  }
+) {
+  try {
+    const userId = await getUserId();
+
+    if (!userId)
+      return NextResponse.json({ error: "No user id" }, { status: 401 });
+
+    const project = await prisma.project.findUnique({
+      where: {
+        id: params.id,
+      },
+    });
+
+    if (!project)
+      return NextResponse.json({ error: "No project found." }, { status: 404 });
+
+    const payload = {
+      projectId: project.id,
+      upvoterId: userId,
+    };
+
+    const hasUpvoted = await prisma.upvote.findFirst({
+      where: payload,
+    });
+
+    if (hasUpvoted) {
+      await prisma.upvote.delete({
+        where: {
+          id: hasUpvoted.id,
+        },
+      });
+    } else {
+      await prisma.upvote.create({
+        data: payload,
+      });
+    }
+
+    return NextResponse.json(
+      {
+        message: `Project has been ${hasUpvoted ? "un" : ""}upvoted`,
+      },
+      {
+        status: hasUpvoted ? 200 : 201,
+      }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET(
+  request: Request,
+  {
+    params,
+  }: {
+    params: { id: string };
+  }
+) {
+  try {
+    const userId = await getUserId();
+
+    if (!userId)
+      return NextResponse.json({ error: "No user id" }, { status: 401 });
+
+    const project = await prisma.project.findUnique({
+      where: {
+        id: params.id,
+      },
+    });
+
+    if (!project)
+      return NextResponse.json({ error: "No project found." }, { status: 404 });
+
+    const payload = {
+      projectId: project.id,
+      upvoterId: userId,
+    };
+
+    const hasUpvoted = await prisma.upvote.findFirst({
+      where: payload,
+    });
+
+    return NextResponse.json(
+      {
+        hasUpvoted: hasUpvoted ? true : false,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
