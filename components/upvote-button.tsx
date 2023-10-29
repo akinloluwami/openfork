097a990b13e69b865cb64f9133c5d442f5f310c5
akinloluwami <akinkunmioye42@gmail.com>
2023-10-30 00:30:30 +0100

    feat: renamed prop and updated styling

--- components/upvote-button.tsx
+++ components/upvote-button.tsx
@@ -4,27 +4,27 @@ import { useEffect, useState } from "react";
 import { Button } from "./ui/button";
 import { axios } from "@/lib/axios";
 
-const Upvotebutton = ({ id }: { id: string }) => {
+const Upvotebutton = ({ projectId: projectId }: { projectId: string }) => {
   const [hasUpvoted, setHasUpvoted] = useState(false);
 
   const upvote = async () => {
     setHasUpvoted(!hasUpvoted);
     try {
-      await axios.post(`/project/${id}/upvote`);
+      await axios.post(`/project/${projectId}/upvote`);
     } catch (error) {
       setHasUpvoted(false);
     }
   };
 
   useEffect(() => {
-    axios(`/project/${id}/upvote`).then((data) => {
+    axios(`/project/${projectId}/upvote`).then((data) => {
       setHasUpvoted(data.data.hasUpvoted);
     });
   }, []);
 
   return (
     <Button
-      className={`w-[250px] py-6 mt-5 font-bold ${
+      className={`w-full py-6 font-bold ${
         hasUpvoted
           ? "border-2 text-black border-black shadow-none bg-transparent hover:text-white hover:bg-black/80"
           : "border-transparent border-2"
