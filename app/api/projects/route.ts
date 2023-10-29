e7318cce5e5f1c6789cbf5f388c69be75686fa2a
akinloluwami <akinkunmioye42@gmail.com>
2023-10-30 00:03:25 +0100

    feat: get projects

---
+++ app/api/projects/route.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function GET(request: Request) {
+  try {
+    const projects = await prisma.project.findMany({
+      include: {
+        user: {
+          select: {
+            username: true,
+            isVerified: true,
+          },
+        },
+        _count: {
+          select: {
+            upvotes: true,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(
+      {
+        projects,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: "Interval server error.",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
