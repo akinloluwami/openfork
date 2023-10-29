14b8ac520038e2d5cf3d41383e178b8a23bac387
akinloluwami <akinkunmioye42@gmail.com>
2023-10-30 00:34:04 +0100

    feat: linked card to project page

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Avatar } from "./ui/avatar";
 import Upvotebutton from "./upvote-button";
 
@@ -21,14 +22,16 @@ const ProjectCard = ({
 
   return (
     <div className="border-2 border-black rounded-lg w-[300px] p-2">
-      <h2 className="font-semibold text-xl">{name}</h2>
-      <p className="mt-3">{description}</p>
-      <div className="flex mt-3">
-        <p className="font-semibold text-sm">{username}</p>
-      </div>
-      <div className="mt-10">
-        <Upvotebutton projectId={id} />
-      </div>
+      <Link href={`/${username}/${slug}`}>
+        <h2 className="font-semibold text-xl">{name}</h2>
+        <p className="mt-3">{description}</p>
+        <div className="flex mt-3">
+          <p className="font-semibold text-sm">{username}</p>
+        </div>
+        <div className="mt-10">
+          <Upvotebutton projectId={id} />
+        </div>
+      </Link>
     </div>
   );
 };
