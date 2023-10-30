3f9e52eb10934f766455885ccba76c87056ab800
akinloluwami <akinkunmioye42@gmail.com>
2023-10-30 02:32:07 +0100

    feat: updated styling

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -21,16 +21,20 @@ const ProjectCard = ({
   const { username, isVerified } = user;
 
   return (
-    <div className="border-2 border-black rounded-lg w-[300px] p-2">
-      <Link href={`/${username}/${slug}`}>
-        <h2 className="font-semibold text-xl">{name}</h2>
-        <p className="mt-3">{description}</p>
-        <div className="flex mt-3">
-          <p className="font-semibold text-sm">{username}</p>
+    <div className="border-2 border-black rounded-lg w-[300px] p-2 h-[230px]">
+      <Link
+        href={`/${username}/${slug}`}
+        className="flex flex-col justify-between h-full"
+      >
+        <div className="">
+          <h2 className="font-semibold text-xl">{name}</h2>
+          <p className="mt-3">{description}</p>
+          <div className="flex mt-3">
+            <p className="font-semibold text-sm">{username}</p>
+          </div>
         </div>
-        <div className="mt-10">
-          <Upvotebutton projectId={id} />
-        </div>
+
+        <Upvotebutton projectId={id} />
       </Link>
     </div>
   );
